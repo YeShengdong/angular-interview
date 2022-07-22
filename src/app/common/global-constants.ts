63a280f05f68fcf5c9f67d4dd4f1aee5dab5f779
FynSat <fynsat@gmail.com>
2022-07-23 02:35:15 +0530

    created files with app constants

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,6 +1,7 @@
-export class AppGlobalConstants{
+export class AppGlobalConstants {
 
-    public static CHECKBOXESLABELNAME:string = "Status";
-    public static CHECKBOXESCOLUMNS:number=1;
-
+    public static CHECKBOXESLABELNAME: string = "Status";
+    public static CHECKBOXESCOLUMNSBYDEFAULT: number = 2;
+    public static SELECTALLTEXT: string = "Select All";
+    public static SELECTALLVALUE: string = "-1";
 }
