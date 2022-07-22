b61cd84361303aea96554a8640fe14e3796cc587
FynSat <fynsat@gmail.com>
2022-07-23 02:50:57 +0530

    added Explanation file

---
+++ src/explaination.ts
@@ -0,0 +1,8 @@
+//Explanation
+//      1:> I have created created a directive to handle the splitting of multiple check boxes if the value of the columns >1
+//           ie. if it is 2 ,then there would be two columns of the check box and so on and so forth
+//      2:> I have not changed the name of the parameters in multi -check component.
+//      3:>I have created a common folder which contains a file with constant values like columns , 'Select All' & its 
+//         corresponding value & the component name -'Status'.
+//      4:> I have created directives under shared folder.
+//      5:>I have added petinent comments where ever required
